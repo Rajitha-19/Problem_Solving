3a42e15edc2309bbf090a1740a173e537070265d
Rajitha-19 <72659413+Rajitha-19@users.noreply.github.com>
2021-07-16 10:51:55 +0530

    Create DFS.java

---
+++ Graphs/DFS.java
@@ -0,0 +1,55 @@
+  
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class DFS
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>>adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer>ans = obj.dfsOfGraph(V, adj);
+            for (int i =0 ;i < ans.size (); i++) 
+                System.out.print (ans.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        ArrayList<Integer> result=new ArrayList<>();
+        boolean[] visited=new boolean[V];
+        dfs(0,adj,result,visited);
+        return result;
+    }
+    public void dfs(int value, ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> result,boolean[] visited){
+        visited[value]=true;
+        result.add(value);
+        for(Integer neighbour:adj.get(value)){
+            if(!visited[neighbour]){
+                dfs(neighbour,adj,result,visited);
+            }
+        }
+    }
+}
