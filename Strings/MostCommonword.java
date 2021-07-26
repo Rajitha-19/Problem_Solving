df92d035a55e8fc08c9ad6e474b08881023063c4
Rajitha-19 <72659413+Rajitha-19@users.noreply.github.com>
2021-07-26 17:02:35 +0530

    Update MostCommonword.java

--- Strings/MostCommonword.java
+++ Strings/MostCommonword.java
@@ -37,3 +37,56 @@ class Solution {
         return "";
     }
 }
+
+/*
+Given an array arr containing N words consisting of lowercase characters. Your task is to find the most frequent word in the array. If multiple words have same frequency, then print the word whose first occurence occurs last in the array as compared to the other strings with same frequency.
+
+Example 1:
+
+Input:
+N = 3
+arr[] = {geeks,for,geeks}
+Output: geeks
+Explanation: "geeks" comes 2 times.
+Example 2:
+
+Input:
+N = 2
+arr[] = {hello,world}
+Output: world
+Explanation: "hello" and "world" both
+have 1 frequency. We print world as
+its first occurence comes last in the
+input array.
+*/
+
+class Solution
+{
+    //Function to find most frequent word in an array of strings.
+    public String mostFrequentWord(String arr[],int n)
+    {
+        // code here
+        LinkedHashMap<String,Integer> map=new LinkedHashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            list.add(entry.getValue());
+        }
+        int k=Collections.max(list);
+        ArrayList<String> ans=new ArrayList<>();
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            if(entry.getValue()==k){
+                ans.add(entry.getKey());
+            }
+        }
+        return ans.get(ans.size()-1);
+    }
+
+}
